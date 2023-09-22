801192562904ebd350086be9a2994cb48ff6e6a6
GrahamBuhrman <57652495+GrahamBuhrman@users.noreply.github.com>
2023-09-22 10:38:24 -0500

    Update plots and analyses

--- Results Scripts/Read Data.R
+++ Results Scripts/Read Data.R
@@ -3,16 +3,16 @@
 require(dplyr)
 require(purrr)
 
-sim_results_dat_list <- list.files(path = "C:/Users/gwbuh/Desktop/Non-parametric Condor Results/Sim Results 9-7-23", pattern = ".rds", full.names = TRUE) 
+sim_results_dat_list <- list.files(path = "C:/Users/gwbuh/Desktop/Non-parametric Condor Results/Sim Results 9-20-23", pattern = ".rds", full.names = TRUE) 
 
 
-sim_results_dat_1 <- map_dfr(sim_results_dat_list[1:8000], readRDS)
+sim_results_dat_1 <- map_dfr(sim_results_dat_list[1:9000], readRDS)
 gc()
 sim_results_dat_2 <- map_dfr(sim_results_dat_list[8001:16000], readRDS)
 gc()
 sim_results_dat_3 <- map_dfr(sim_results_dat_list[16001:24000], readRDS)
 gc()
 
-sim_results_dat <- rbind(sim_results_dat_1, sim_results_dat_2, sim_results_dat_3)
+sim_results_dat <- rbind(sim_results_dat_1)
 
-saveRDS(sim_results_dat, "Raw Sim Data/sim_results_dat_9-7-23.rds")
+saveRDS(sim_results_dat_1, "Raw Sim Data/sim_results_dat_9-20-23.rds")
--- Simulation Scripts/Analysis Functions.R
+++ Simulation Scripts/Analysis Functions.R
@@ -7,32 +7,21 @@ require(tidyr)
 require(lme4)
 require(simhelpers)
 require(ggplot2)
+require(ggpubr)
+require(cowplot)
 
 # Summary Statistics ------------------------------------------------------
-
-get_summary_stats <- function(sim_results) {
-  
-  sim_results %>%
-    dplyr::group_by(RCT, ICC, J, submit) %>%
-    dplyr::summarize(CATE_TRUE = mean(ITE),
-                     CATE_CF = mean(ITE_CF),
-                     CATE_stan4bart = mean(ITE_stan4bart),
-                     CATE_BCF = mean(ITE_BCF),
-                     CATE_MBCF = mean(ITE_MBCF)) %>%
-    dplyr::ungroup()
-  
-}
 
 get_PEHE_stats <- function(sim_results) {
 
   sim_results %>%
-    dplyr::group_by(RCT, J, submit) %>%
+    dplyr::group_by(ICC, J, submit) %>%
     dplyr::summarize(PEHE_CF = sqrt(mean((ITE - ITE_CF)^2)),
                      PEHE_stan4bart = sqrt(mean((ITE - ITE_stan4bart)^2)),
                      PEHE_BCF = sqrt(mean((ITE - ITE_BCF)^2)),
                      PEHE_MBCF = sqrt(mean((ITE - ITE_MBCF)^2))) %>%
     dplyr::ungroup() %>% 
-    group_by(RCT, J) %>% 
+    group_by(ICC, J) %>% 
     summarize(PEHE_CF_EST = mean(PEHE_CF), 
               PEHE_CF_SD = sd(PEHE_CF), 
               PEHE_stan4bart_EST = mean(PEHE_stan4bart), 
@@ -44,37 +33,19 @@ get_PEHE_stats <- function(sim_results) {
   
 }
 
-# Performance Calculations ------------------------------------------------
-
-get_abs_perf <- function(sim_results) {
+get_PEHE_agg <- function(sim_results) {
   
-  summary_stats <- get_summary_stats(sim_results)
-  
-  res_dat <- 
-    summary_stats %>%
-    tidyr::pivot_longer(cols = c(CATE_CF, CATE_stan4bart, CATE_BCF, CATE_MBCF), 
-                        names_prefix = "CATE_", 
-                        names_to = "Method", 
-                        values_to = "Estimate") %>% 
-    dplyr::group_by(Method, RCT, ICC, J) %>% 
-    do(simhelpers::calc_absolute(., estimates = Estimate, true_param = CATE_TRUE)) %>%
-    dplyr::ungroup()
-  
-}
-
-get_abs_perf_agg <- function(sim_results) {
-  
-  summary_stats <- get_summary_stats(sim_results)
-  
-  res_dat <- 
-    summary_stats %>%
-    tidyr::pivot_longer(cols = c(CATE_CF, CATE_stan4bart, CATE_BCF, CATE_MBCF), 
-                        names_prefix = "CATE_", 
-                        names_to = "Method", 
-                        values_to = "Estimate") %>% 
-    dplyr::group_by(Method, RCT, J) %>% 
-    do(simhelpers::calc_absolute(., estimates = Estimate, true_param = CATE_TRUE)) %>%
-    dplyr::ungroup()
+  sim_results %>%
+    dplyr::group_by(ICC, J, submit) %>%
+    dplyr::summarize(PEHE_CF = sqrt(mean((ITE - ITE_CF)^2)),
+                     PEHE_stan4bart = sqrt(mean((ITE - ITE_stan4bart)^2)),
+                     PEHE_BCF = sqrt(mean((ITE - ITE_BCF)^2)),
+                     PEHE_MBCF = sqrt(mean((ITE - ITE_MBCF)^2))) %>%
+    dplyr::ungroup() %>%
+    tidyr::pivot_longer(cols = PEHE_CF:PEHE_MBCF,
+                        names_prefix = "PEHE_",
+                        names_to = "method",
+                        values_to = "PEHE")
   
 }
 
@@ -87,12 +58,12 @@ get_agg_results <- function(sim_results) {
                         names_prefix = "ITE_",
                         names_to = "method",
                         values_to = "sampleITE") %>%
-    dplyr::group_by(RCT, ICC, J, method, IDSTU) %>%
+    dplyr::group_by(ICC, J, method, IDSTU) %>%
     dplyr::summarize(estITE = mean(sampleITE),
                      lbITE = quantile(sampleITE, 0.025),
                      ubITE = quantile(sampleITE, 0.975)) %>%
     dplyr::ungroup() %>%
-    dplyr::select(IDSTU, RCT, ICC, J, method, estITE, lbITE, ubITE)
+    dplyr::select(IDSTU, ICC, J, method, estITE, lbITE, ubITE)
   
 }
 
@@ -103,12 +74,12 @@ get_agg_results_plot <- function(sim_results) {
                         names_prefix = "ITE_",
                         names_to = "method",
                         values_to = "sampleITE") %>%
-    dplyr::group_by(RCT, J, method, IDSTU) %>%
+    dplyr::group_by(J, method, IDSTU) %>%
     dplyr::summarize(estITE = mean(sampleITE),
                      lbITE = quantile(sampleITE, 0.025),
                      ubITE = quantile(sampleITE, 0.975)) %>%
     dplyr::ungroup() %>%
-    dplyr::select(IDSTU, RCT, J, method, estITE, lbITE, ubITE)
+    dplyr::select(IDSTU, J, method, estITE, lbITE, ubITE)
   
 }
 
@@ -124,7 +95,7 @@ get_plot_dat <- function(sim_results) {
   
   plot_dat <-
     dplyr::left_join(agg_results, cov_dat, by = "IDSTU") %>%
-    dplyr::select(IDSCH, IDSTU, RCT, J, method, LIKEMATH, estITE, lbITE, ubITE)
+    dplyr::select(IDSCH, IDSTU, J, method, LIKEMATH, estITE, lbITE, ubITE)
   
   gc()
   
@@ -135,16 +106,27 @@ get_plot_dat <- function(sim_results) {
 
 # Visualize Data ----------------------------------------------------------
 
-make_perf_plot <- function(perf_data, grid_spec_1, grid_spec_2, x_axis_spec, perf_spec) {
+make_PEHE_boxplot <- function(sim_results) {
   
-  ggplot(data = perf_data, 
-         aes(x = x_axis_spec, 
-             y = perf_spec, 
-             color = Method, 
-             group = Method)) + 
-    geom_point() + 
-    geom_line() + 
-    facet_grid(cols = vars(grid_spec_1), rows = vars(grid_spec_2))
+  PEHE_agg_dat <- get_PEHE_agg(sim_results)
+  
+  PEHE_agg_dat$method <- factor(PEHE_agg_dat$method, levels = c("CF", "stan4bart", "BCF", "MBCF"))
+  PEHE_agg_dat$Clusters <- factor(PEHE_agg_dat$J)
+    
+  ggplot(PEHE_agg_dat,
+         aes(x = method,
+             y = PEHE,
+             fill = method)) +
+    geom_boxplot(alpha = 0.5, size = 0.75) +
+    geom_hline(yintercept = 0.2, linetype = "dashed", linewidth = 1) +
+    scale_fill_manual(values=c("#0479a8", "#c5050c", "#f7941e","#97b85f")) +
+    facet_grid(ICC~Clusters, labeller = labeller(ICC = label_both, Clusters = label_both)) +
+    theme_bw() +
+    labs(x = "Method",
+         y = "RPEHE",
+         fill = "Method") +
+    theme(text = element_text(size = 25, color = "black", face = "plain"),
+          legend.position = "none")
   
 }
 
@@ -171,32 +153,81 @@ make_agg_CATE_plot <- function(plot_data, method_spec, color_spec){
   
   plot_data_filter <- 
     plot_data %>%
-    dplyr::filter(., J == 200, RCT == FALSE, method == method_spec)
+    dplyr::filter(., J == 200, method == method_spec)
   
   ggplot(data = plot_data_filter,
-         aes(x = LIKEMATH)) +
+         aes(x = LIKEMATH,
+             fill = method_spec)) +
     ylim(0, 2.25) +
     xlim(5, 14) +
-    stat_function(fun = fun1, n = 5001, linewidth = 3, linetype = "solid", color = "black") +
-    stat_function(fun = fun2, n = 5001, linewidth = 3, linetype = "solid", color = "black") +
-    stat_function(fun = fun3, n = 5001, linewidth = 3, linetype = "solid", color = "black") +
+    stat_function(fun = fun1, n = 5001, linewidth = 2, linetype = "solid", color = "black") +
+    stat_function(fun = fun2, n = 5001, linewidth = 2, linetype = "solid", color = "black") +
+    stat_function(fun = fun3, n = 5001, linewidth = 2, linetype = "solid", color = "black") +
     geom_ribbon(aes(ymin = predict(mgcv::gam(lbITE ~ s(LIKEMATH, bs = "cs"))), 
                     ymax = predict(mgcv::gam(ubITE ~ s(LIKEMATH, bs = "cs")))),
                 alpha = 0.25,
                 fill = color_spec,
                 color = color_spec,
-                linewidth = 2,
+                linewidth = 1.5,
                 linetype = "dotdash") + 
-    geom_smooth(aes(y = estITE), color = color_spec, linewidth = 2, method = "gam", se = FALSE) +
+    geom_smooth(aes(y = estITE), color = color_spec, linewidth = 1.5, method = "gam", se = FALSE) +
     theme_bw() +
-    theme(legend.position = "none",
-          axis.text = element_text(size = 30, family = "Verdana", color = "black"),
-          axis.title = element_blank())
+    theme(text = element_text(size = 15, color = "black"),
+          legend.position = "top",
+          axis.title = element_blank()) +
+    labs(fill = "Method")
   
 }
 
+arrange_CATE_plots <- function(plot_1, plot_2, plot_3, plot_4) {
+  
+  figure <- ggarrange(plot_1, plot_2, plot_3, plot_4, 
+                      ncol = 2, nrow = 2,
+                      label.x = 0,
+                      label.y = 0,
+                      hjust = -1.5,
+                      vjust = -5,
+                      align = "hv",
+                      legend = "top",
+                      font.label = list(size = 15, face = "plain"))
+  
+  annotate_figure(figure, left = ggpubr::text_grob("CATE", rot = 90, size = 30),
+                  bottom = ggpubr::text_grob("Like Math", size = 30))
+  
+}
+  
 
-# get different plots
+# get arranged cate plots
+sim_plot_dat <- get_plot_dat(sim_results_dat)
+test_CF_plot <- make_agg_CATE_plot(plot_data = sim_plot_dat, method_spec = "CF", color_spec = "#0479a8")
+test_stan4bart_plot <- make_agg_CATE_plot(plot_data = sim_plot_dat, method_spec = "stan4bart", color_spec = "#c5050c")
+test_BCF_plot <- make_agg_CATE_plot(plot_data = sim_plot_dat, method_spec = "BCF", color_spec = "#f7941e")
+test_MBCF_plot <- make_agg_CATE_plot(plot_data = sim_plot_dat, method_spec = "MBCF", color_spec = "#97b85f")
+
+png(filename="Images/CATE_plots.png",
+    type="cairo",
+    units="in", 
+    width=12, 
+    height=12, 
+    pointsize=12, 
+    res=300)
+arrange_CATE_plots(test_CF_plot, test_stan4bart_plot, test_BCF_plot, test_MBCF_plot)
+dev.off()
+
+# get boxplot
+png(filename="Images/RPEHE_boxplot.png",
+    type="cairo",
+    units="in", 
+    width=16, 
+    height=12, 
+    pointsize=12, 
+    res=300)
+make_PEHE_boxplot(sim_results_dat)
+dev.off()
+
+##########################################################################################################
+
+# get arranged CATE plots
 png(filename="Images/CF_plot_black_3.png", 
     type="cairo",
     units="in", 
